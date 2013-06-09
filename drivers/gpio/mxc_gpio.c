794b1f5bf0fd1c7613b6a035904f8f00e3ae7f54
Nick Spence <rxtd10@freescale.com>
2013-06-09 12:37:47 -0700

    Modify mxc_gpio.c to remove device specific defines
    Signed-off-by: Rahul Bhatia <b44338@freescale.com>
    Change-Id: Idb1ee95135916fc634e45a282cdec644e6e11cec
    Reviewed-on: http://git.am.freescale.net:8181/2942
    Reviewed-by: Singh Jaiprakash-B44839 <jaiprakash.singh@freescale.com>
    Tested-by: Singh Jaiprakash-B44839 <jaiprakash.singh@freescale.com>
    Reviewed-on: http://git.am.freescale.net:8181/2953

--- drivers/gpio/mxc_gpio.c
+++ drivers/gpio/mxc_gpio.c
@@ -5,6 +5,8 @@
  * Copyright (C) 2011
  * Stefano Babic, DENX Software Engineering, <sbabic@denx.de>
  *
+ * Copyright (C) 2013 Freescale Semiconductor, Inc.
+ *
  * See file CREDITS for list of people who contributed to this
  * project.
  *
@@ -41,15 +43,16 @@ static unsigned long gpio_ports[] = {
 	[0] = GPIO1_BASE_ADDR,
 	[1] = GPIO2_BASE_ADDR,
 	[2] = GPIO3_BASE_ADDR,
-#if defined(CONFIG_MX25) || defined(CONFIG_MX27) || defined(CONFIG_MX51) || \
-		defined(CONFIG_MX53) || defined(CONFIG_MX6)
+#ifdef GPIO4_BASE_ADDR
 	[3] = GPIO4_BASE_ADDR,
 #endif
-#if defined(CONFIG_MX27) || defined(CONFIG_MX53) || defined(CONFIG_MX6)
+#ifdef GPIO5_BASE_ADDR
 	[4] = GPIO5_BASE_ADDR,
+#endif
+#ifdef GPIO6_BASE_ADDR
 	[5] = GPIO6_BASE_ADDR,
 #endif
-#if defined(CONFIG_MX53) || defined(CONFIG_MX6)
+#ifdef GPIO7_BASE_ADDR
 	[6] = GPIO7_BASE_ADDR,
 #endif
 };
