9c7a141cd6bee584cb194574a8768b52b42873a7
Arpit Goel <B44344@freescale.com>
2013-09-12 14:47:27 +0530

    Adds support to patch CPRI MAC address in Linx DTB.
    This patch parses environment variables of CPRI MAC address
    from u-boot. CPRI MAC address environment variables will be
    of the from cpri<cpri-idx>eth<eth-idx>addr.
    
    Signed-off-by: Arpit Goel <B44344@freescale.com>
    Change-Id: Ieba63a402f6fd31eb46886fc449252801fc9359f
    Reviewed-on: http://git.am.freescale.net:8181/4692
    Reviewed-by: Spence Nicholas J-RXTD10 <Nick.Spence@freescale.com>
    Reviewed-by: Singh Jaiprakash-B44839 <jaiprakash.singh@freescale.com>
    Tested-by: Singh Jaiprakash-B44839 <jaiprakash.singh@freescale.com>

--- arch/arm/cpu/armv7/d4400/fdt.c
+++ arch/arm/cpu/armv7/d4400/fdt.c
@@ -27,8 +27,47 @@
 #include <libfdt.h>
 #include <fdt_support.h>
 
+static void fdt_fixup_cpri_ethernet(void *fdt)
+{
+	int node, i, j;
+	char enet[16], *tmp, *end;
+	char mac[16] = "cpri0eth0addr";
+	const char *path;
+	unsigned char mac_addr[6];
+
+	node = fdt_path_offset(fdt, "/aliases");
+	if (node < 0)
+		return;
+
+	i = 0;
+	while ((tmp = getenv(mac)) != NULL) {
+		sprintf(enet, "cpri%dethernet%d", (i>>1), (i%2));
+		path = fdt_getprop(fdt, node, enet, NULL);
+		if (!path) {
+			debug("No alias for %s\n", enet);
+			i++;
+			sprintf(mac, "cpri%deth%daddr", (i>>1), (i%2));
+			continue;
+		}
+
+		for (j = 0; j < 6; j++) {
+			mac_addr[j] = tmp ? simple_strtoul(tmp, &end, 16) : 0;
+			if (tmp)
+				tmp = (*end) ? end+1 : end;
+		}
+
+		do_fixup_by_path(fdt, path, "mac-address", &mac_addr, 6, 0);
+		do_fixup_by_path(fdt, path, "local-mac-address",
+				 &mac_addr, 6, 1);
+
+		i++;
+		sprintf(mac, "cpri%deth%daddr", (i>>1), (i%2));
+	}
+}
+
 void ft_cpu_setup(void *blob, bd_t *bd)
 {
 	fdt_fixup_ethernet(blob);
+	fdt_fixup_cpri_ethernet(blob);
 }
 
